42cba3b2106b7bafb2471ba3f3c3eea67b21a5ef
Ermmoha <ermoh_00@mail.ru>
2024-05-29 07:01:04 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,28 @@ import java.io.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        try(ObjectOutputStream obs = new ObjectOutputStream(new FileOutputStream("text.txt")); ObjectInputStream ois = new ObjectInputStream((new FileInputStream("text.txt")))){
+        // Создаем объект, записывающий объекты в поток
+        try(ObjectOutputStream obs = new ObjectOutputStream(new FileOutputStream("text.txt"));
+            // Создаем объект, достающий объекты из потока
+            ObjectInputStream ois = new ObjectInputStream((new FileInputStream("text.txt")))){
+            // Ввод с консоли
             Scanner input = new Scanner(System.in);
             System.out.println("Введите число");
             boolean checker = true;
             Double x = input.nextDouble();
+            //Пока чекер истин
             while (checker) {
-                System.out.println("Введите команду");
+                System.out.println("Введите команду (save/upload):");
                 String in = input.next();
+                // Создаем объект класса Solver, передавая значение введенное с консоли
                 Solver solver = new Solver(x);
+                //Проверяем, была ли введена команда save. (Если да, то solver записываем в поток ввода.
                 if (in.equals("save")) {
                     obs.writeObject(solver);
                     System.out.println("Сохранено");
                     checker = true;
                 }
+                //проверяем, была ли введена upload. Если да, то проиходит чтение и изменение cheker на false.
                 if (in.equals("upload")) {
                     Solver object = (Solver) ois.readObject();
                     double y = object.x;
